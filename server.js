f957ede61094bb6ccf925f8cefcddfc38939e172
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-10-24 16:36:56 -0700

    cp dines

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const port = process.env.port || 3000;
 
-// Proxy configuration
-const apiProxy = createProxyMiddleware({
-  target:
-    "https://t461i9k5lkzb-dc118956-dcbb-4fae-b4ef-0a8390fe1256.tunnel.runloop.ai",
-  changeOrigin: true,
-  ws: true, // Enable WebSocket proxying
-  //   pathRewrite: {
-  //     //"^/api/old-path": "/api/new-path", // Optional path rewrite
-  //   },
-  onError: (err, req, res) => {
-    console.error("Proxy error:", err);
-    res.status(500).send("Proxy error");
-  },
-});
-
 // Apply middleware for the API path
-app.use("/", apiProxy);
+app.use("/:tunnelToken", (res, res, next) => {
+  const { tunnelToken } = req.params;
+  // Construct the target URL using the tunnelToken
+  const targetUrl = `https://${tunnelToken}.tunnel.runloop.ai`;
+
+  const apiProxy = createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+    ws: true, // Enable WebSocket proxying
+    onError: (err, req, res) => {
+      console.error("Proxy error:", err);
+      res.status(500).send("Proxy error");
+    },
+  });
+  apiProxy(req, res, next); // Apply the proxy middleware
+});
 
 // This will handle HTTP GET requests to any path
 app.get("*", (req, res) => {
