ccee86bfbc366cfb8a71b0aa51bf95d2c7b28f91
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-10-24 18:52:45 -0700

    cp dines

--- server.js
+++ server.js
@@ -4,34 +4,33 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Apply middleware for the API path
-app.use("/:tunnelToken/proxy/*", (req, res, next) => {
-  const { tunnelToken } = req.params;
-  // Construct the target URL using the tunnelToken
-  const targetUrl = `https://${tunnelToken}.tunnel.runloop.ai`;
-
-  const apiProxy = createProxyMiddleware({
-    target: targetUrl,
-    changeOrigin: true,
-    pathRewrite: {
-      // Rewrite the path to remove '/:tunnelToken/proxy/' and keep everything after
-      "^/[^/]+/proxy/(.*)$": "/$1",
-    },
-    //selfHandleResponse: true, // necessary to modify the response
-
-    ws: true, // Enable WebSocket proxying
-    onError: (err, req, res) => {
-      console.error("Proxy error:", err);
-      res.status(500).send("Proxy error");
-    },
-  });
-  apiProxy(req, res, next); // Apply the proxy middleware
+// Create a single instance of createProxyMiddleware
+const apiProxy = createProxyMiddleware({
+  changeOrigin: true,
+  pathRewrite: {
+    // Rewrite the path to remove '/:tunnelToken/proxy/' and keep everything after
+    "^/[^/]+/proxy/(.*)$": "/$1",
+  },
+  ws: true, // Enable WebSocket proxying
+  onError: (err, req, res) => {
+    console.error("Proxy error:", err);
+    res.status(500).send("Proxy error");
+  },
+  router: (req) => {
+    const { tunnelToken } = req.params;
+    // Construct the target URL using the tunnelToken
+    return `https://${tunnelToken}.tunnel.runloop.ai`;
+  },
 });
+
+// Apply middleware for the API path
+app.use("/:tunnelToken/proxy/*", apiProxy);
 
 // This will handle HTTP GET requests to any path
 app.get("/", (req, res) => {
   res.send("This is a proxy server. Does not serve content directly.");
 });
+
 // This will handle HTTP GET requests to any path
 app.get("/health", (req, res) => {
   res.send("Super healthy");
