a21dcbbc09fddbd1a368c1f567e492e2a5a9a4f3
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-10-24 20:22:28 -0700

    cp dines

--- server.js
+++ server.js
@@ -1,60 +1,35 @@
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const httpProxy = require("http-proxy");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Function to extract the tunnelToken from the request URL
-function extractToken(req) {
-  // Assuming URL pattern is "/:tunnelToken/proxy/*"
-  const match = req.url.match(/\/([^\/]+)\/proxy/);
-  return match ? match[1] : null;
-}
-// Create a single instance of createProxyMiddleware
-const apiProxy = createProxyMiddleware({
+// Create a proxy server with http-proxy
+const proxy = httpProxy.createProxyServer({
   target:
-    "https://l7srq4mnqdnc-dc118956-dcbb-4fae-b4ef-0a8390fe1256.tunnel.runloop.ai",
-  changeOrigin: true,
-  pathRewrite: {
-    // Rewrite the path to remove '/:tunnelToken/proxy/' and keep everything after
-    "^/[^/]+/proxy/(.*)$": "/$1",
-  },
-  ws: true, // Enable WebSocket proxying
-  toProxy: true, // Enable the proxy to be used in a reverse proxy
-  onError: (err, req, res) => {
-    console.error("Proxy error:", err);
-    res.status(500).send("Proxy error");
-  },
-  router: (req) => {
-    const tunnelToken = extractToken(req);
-    if (!tunnelToken) {
-      console.error("Failed to extract tunnelToken from URL:", req.url);
-      return null;
-    }
-    return `https://${tunnelToken}.tunnel.runloop.ai`;
-  },
-});
-
-// Apply middleware for the API path
-// app.use("/:tunnelToken/proxy", apiProxy);
-//app.use("/:tunnelToken/proxy/*", apiProxy);
-const apiProxySimple = createProxyMiddleware({
-  target:
-    "https://zdj8yeshcf7z-dc118956-dcbb-4fae-b4ef-0a8390fe1256.tunnel.runloop.ai",
+    "https://x7xejlw7ppim-dc118956-dcbb-4fae-b4ef-0a8390fe1256.tunnel.runloop.ai",
   changeOrigin: true,
   ws: true, // Enable WebSocket proxying
-  //toProxy: true, // Enable the proxy to be used in a reverse proxy
-  onError: (err, req, res) => {
-    console.error("Proxy error:", err);
-    res.status(500).send("Proxy error");
-  },
 });
-// This will handle HTTP GET requests to any path
-app.use("/*", apiProxySimple);
 
-// This will handle HTTP GET requests to any path
+proxy.on("error", (err, req, res) => {
+  console.error("Proxy error:", err);
+  res.status(500).send("Error in proxy operation");
+});
+
+// Proxy all requests
+app.use((req, res) => {
+  proxy.web(req, res);
+});
+
+// Optionally handle WebSocket requests
+app.on("upgrade", (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
+
+// Health check route
 app.get("/health", (req, res) => {
-  res.send("Super healthy");
+  res.send("Server is healthy");
 });
 
 // Start the server
